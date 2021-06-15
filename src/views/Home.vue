6917c45606f6f17756bd2fd861a612bd7fe33d2f
Randy Rodriguez <Randyr2125@msn.com>
2021-06-14 23:13:11 -0400

    Initial commit for refactoring site into vue.js

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+<style>
+/* Set the color scheme of the page according to the users OS settings */
+@media (prefers-color-scheme: dark) {
+  body {
+    color: #eee;
+    background: #121212;
+  }
+  a {
+    color: #ffffff;
+  }
+  .nameLogoBlack {
+    display: none;
+  }
+}
+</style>
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
