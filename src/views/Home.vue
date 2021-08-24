924bcf7a04194bc759a5e74a7d085c42a3097bf1
Randy Rodriguez <Randyr2125@msn.com>
2021-08-24 00:54:37 -0400

    updated nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -80,13 +80,6 @@
                 </ul>
               </b-row>
             </b-card>
-
-            <!-- <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-              <b-card-text>This card has supporting text below as a natural lead-in to additional content.</b-card-text>
-              <template #footer>
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </template>
-            </b-card> -->
           </b-card-group>
         </div>
       </div>
