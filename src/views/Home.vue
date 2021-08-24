0b5caf2b7e232ec1cdf9a0ebe3d95e3575c4da1b
Randy Rodriguez <Randyr2125@msn.com>
2021-08-24 01:30:29 -0400

    animated social icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -93,6 +93,28 @@
 .socials {
   margin-top: 30px;
   margin-right: 50px;
+  background: transparent;
+  border-radius: 50%;
+
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 1);
+  transform: scale(1);
+  animation: pulse 2s infinite;
+}
+@keyframes pulse {
+  0% {
+    transform: scale(0.95);
+    box-shadow: 0 0 0 0 rgba(209, 208, 209, 0.7);
+  }
+
+  70% {
+    transform: scale(1);
+    box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+  }
+
+  100% {
+    transform: scale(0.95);
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
 }
 .text-bold {
   font-weight: 600 !important;
