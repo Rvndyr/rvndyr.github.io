c45b66175ec6961a39ef8064d9381f1894c2986b
rvndyr <randyr2125@msn.com>
2024-03-23 19:36:28 -0400

    added basic nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
+import Nav from './components/Nav.jsx'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
    <>
-   
+  <Nav />
    </>
   )
 }
