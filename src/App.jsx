ac0ac0fc7866e2c4c5a5df11a8cc6bcf3e6f77e8
rvndyr <randyr2125@msn.com>
2024-03-23 20:28:15 -0400

    created a darkmode button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 import Nav from './components/Nav.jsx'
+import Hero from './components/Hero.jsx'
+import styled from 'styled-components'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
 
   return (
-   <>
-  <Nav />
-   </>
+   <Container darkMode = {darkMode}>
+    <Nav darkModeSetter={setDarkMode} darkModeGetter={darkMode}/>
+    <Hero />
+   </Container>
   )
 }
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: flex-start;
+height:100vh;
+background-color: ${props=>props.darkMode ? '#181818': '#f9f9f9'};
+
+`
 
 export default App
